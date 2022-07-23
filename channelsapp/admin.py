b5210957b08851a327e6d2de608075c58e97ba59
Arhan Chaudhary <arhan.ch@gmail.com>
2022-07-22 18:08:45 -0700

    channel app skeleton

--- channelsapp/admin.py
+++ channelsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Channel, Poll, Choice
 
 # Register your models here.
+admin.site.register(Channel)
+admin.site.register(Poll)
+admin.site.register(Choice)
+
--- channelsapp/models.py
+++ channelsapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Channel(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+class Poll(models.Model):
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    prompt = models.CharField(max_length=500)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice
+
