10d03ccf2ac35f3a91994bbabf7c25486f6be163
Kartik M.P <Kartik.pulipati@gmail.com>
2022-07-23 14:48:46 -0700

    static

--- rallyapp/views.py
+++ rallyapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Petition, Signature
+from .forms import PetitionForm
 
 def home(request):
     return render(request, "rallyapp/home.html")
