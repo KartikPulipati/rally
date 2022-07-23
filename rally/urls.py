f21a176a1f356f017cb76cbfc21b10297f52bdb2
Arhan Chaudhary <arhan.ch@gmail.com>
2022-07-22 17:46:22 -0700

    remove authUser v2

--- rally/urls.py
+++ rally/urls.py
@@ -20,7 +20,4 @@ from rallyapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('auth/', include('authUser.urls')),
-    
-
 ]
---
+++ rallyapp/urls.py
@@ -0,0 +1,5 @@
+from django import path
+
+urlpatterns = [
+
+]
