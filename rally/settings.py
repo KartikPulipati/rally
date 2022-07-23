3c9f655eee2ded2605d7840ba812ec78726e5def
Arhan Chaudhary <arhan.ch@gmail.com>
2022-07-22 18:02:33 -0700

    bugfix

--- rally/settings.py
+++ rally/settings.py
@@ -132,5 +132,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_HOST_USER = "verficationemailbluecoder@gmail.com"
-EMAIL_HOST_PASSWORD = "Verify101"
+EMAIL_HOST_PASSWORD = None
 EMAIL_PORT = 587
