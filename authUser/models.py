9057a219c3a73b00bb58d9aa3f83971f30a0c58a
Kartik M.P <Kartik.pulipati@gmail.com>
2022-07-22 20:44:20 -0700

    Made User model better

--- authUser/models.py
+++ authUser/models.py
@@ -4,18 +4,17 @@ from channelsapp.models import Channel
 
 
 class Member(AbstractUser, PermissionsMixin):
-    email = models.EmailField(unique=True, max_length=255)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    email = models.EmailField(_('email address'), unique=True, max_length=255)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, null=True)
     is_mod = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     FIRST_NAME_FIELD = "first_name"
     LAST_NAME_FIELD = "last_name"
 
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.first_name + " " + self.last_name
-    
