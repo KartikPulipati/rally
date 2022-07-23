f7aefe6e3b9b585adbf90f2e9d26e546dacafed5
Kartik M.P <Kartik.pulipati@gmail.com>
2022-07-23 16:27:47 -0700

    assigned user to correct channel

--- authUser/views.py
+++ authUser/views.py
@@ -5,13 +5,38 @@ from authUser.models import Member
 from authUser.forms import signUpForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from ipware import get_client_ip
+from django.conf import settings
+import requests
+from channelsapp.models import Channel
+
+
+def get_geolocation_for_ip(ip):
+    # url = f"http://api.ipstack.com/{ip}?access_key={settings.IP_API_KEY}"
+    url = f"http://api.ipstack.com/98.207.57.21?access_key={settings.IP_API_KEY}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
 
 
 def signUp(request):
     if request.method == 'POST':
+        ip = get_client_ip(request)
+        geo_info = get_geolocation_for_ip(ip)
         form = signUpForm(request.POST)
         if form.is_valid():
-            user = Member.objects.create_user(username=form.cleaned_data['email'], email=form.cleaned_data['email'], first_name=form.cleaned_data['first_name'], last_name=form.cleaned_data['last_name'], password=form.cleaned_data['password1'])
+            user = form.save(commit=False)
+            try:
+                channel = Channel.objects.get(location=geo_info["zip"])
+            except:
+                channel = Channel(name="dummy", location=geo_info["zip"])
+                channel.save()
+                channel.name = f"Channel #{channel.pk}"
+                channel.save()
+            if channel.member_set.count() < 3:
+                user.is_mod = True
+                
+            user.channel = channel
             user.save()
             login(request, user)
             return redirect('home')
--- channelsapp/views.py
+++ channelsapp/views.py
@@ -7,12 +7,6 @@ import requests
 from .models import Channel, Poll, Choice, UserVote
 from django.conf import settings
 
-def get_geolocation_for_ip(ip):
-    url = f"http://api.ipstack.com/98.207.57.21?access_key={settings.IP_API_KEY}"
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
-
 class ChannelView(View, LoginRequiredMixin):
     login_url = '/login/'
     redirect_field_name = 'redirect_to'
@@ -21,19 +15,8 @@ class ChannelView(View, LoginRequiredMixin):
         ip = get_client_ip(request)
         geo_info = get_geolocation_for_ip(ip)
 
-        try:
-            channel = Channel.objects.get(location=geo_info["zip"])
-        except:
-            channel = Channel(name="dummy", location=geo_info["zip"])
-            channel.save()
-            channel.name = f"Channel #{channel.pk}"
-            channel.save()
-        request.user.channel = channel
-        request.user.save()
-        if channel.member_set.count() < 3:
-            request.user.is_mod = True
-        request.user.save()
-
+        channel = request.user.channel
+        channel = request.user.channel
         messages = channel.message_set.all()
         show_poll_pks = [int(i) for i in request.user.uservote_set.all().values_list('poll', flat=True)]
         return render(request, "channelsapp/channels.html", {"room_name": room_name, "messages": messages, "polls": channel.poll_set.all(),
