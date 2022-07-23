d5c5c513680aff4d2d92635b7242b2092f319ba8
Arhan Chaudhary <arhan.ch@gmail.com>
2022-07-22 17:32:27 -0700

    Delete authUser

--- authUser/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthuserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authUser'
--- authUser/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.signUp, name='signup'),
-    path('email_verification/', views.emailVer, name='emailVer'),
-]
