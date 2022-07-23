af8d2d07bc49811a6a0a1059568f7309bd9f5b41
Arhan Chaudhary <arhan.ch@gmail.com>
2022-07-22 19:23:56 -0700

    Typo

--- channelsapp/views.py
+++ channelsapp/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 def get_geolocation_for_ip(ip):
     print(ip)
-    url = f"http://api.ipstack.com/{ip}?access_key={settings.IPSTACK_API_KEY}"
+    url = f"http://api.ipstack.com/{ip}?access_key={settings.IP_API_KEY}"
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
