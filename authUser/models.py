b8165635bead657e09c8dc56240e10c016d37b62
Kartik M.P <Kartik.pulipati@gmail.com>
2022-07-22 20:32:30 -0700

    Added required name fields

--- authUser/models.py
+++ authUser/models.py
@@ -5,11 +5,17 @@ from channelsapp.models import Channel
 
 class Member(AbstractUser, PermissionsMixin):
     email = models.EmailField(unique=True, max_length=255)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
     is_mod = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['username']
+    FIRST_NAME_FIELD = "first_name"
+    LAST_NAME_FIELD = "last_name"
+
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
         return self.first_name + " " + self.last_name
+    
