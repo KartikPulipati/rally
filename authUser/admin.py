3ce05e7018bbaafd10a6a281b450fe1da6fc232e
Kartik M.P <Kartik.pulipati@gmail.com>
2022-07-23 19:52:00 -0700

    fixed bugs

--- authUser/admin.py
+++ authUser/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from authUser.models import Member
 
-admin.site.register(Member, UserAdmin)
+admin.site.register(Member)
